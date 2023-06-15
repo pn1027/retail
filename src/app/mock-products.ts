2a5da06f3491e8b2cf1504aacc0fa5de2bcfa189
Pallav Raj <103437428+pn1027@users.noreply.github.com>
2023-06-15 15:36:27 +0530

    adding product list

---
+++ src/app/mock-products.ts
@@ -0,0 +1,8 @@
+import { IProduct } from './models/product';
+
+export const Products: IProduct[] = [
+  { id: 116, name: 'x', description:'crocs'},
+  { id: 226, name: 'y', description:'leather shoe'},
+  { id: 336, name: 'z', description:'sports shoe'},
+  { id: 446, name: 'w', description:'sneakers'},
+];
